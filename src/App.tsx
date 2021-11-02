c9d20ee8ba07dcac1c43391319d590342a21fd54
elizavetaspivak <elizabethspivak99@gmail.com>
2021-11-02 16:49:42 +0300

    add

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,18 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import mainImage from './common/assets/image/main.jpg'
 
 function App() {
-  return (
-    <div className="App">
-        fsdfsdfds
+  let mainBlock = {
+    backgroundImage: `url(${mainImage})`,
+    backgroundSize: 'cover'
+  }
 
+  return (
+    <div className="App" style={mainBlock}>
+      <div>
+        <h1>Check you weather</h1>
+      </div>
     </div>
   );
 }
--- src/dal/api/api.tsx
+++ src/dal/api/api.tsx
@@ -3,3 +3,5 @@ import React from 'react'
 const REACT_APP_API_URL = 'https://api.openweathermap.org/data/2.5'
 const REACT_APP_API_KEY = '6e33235518f293531848fa88b54c2e2a'
 const REACT_APP_ICON_URL = 'https://openweathermap.org/img/w'
+
+
